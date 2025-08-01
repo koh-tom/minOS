09f2558eea6f28ef688a27ff21c2d55ade230b4a
koh-tom <223271102+koh-tom@users.noreply.github.com>
2025-08-01 23:34:04 +0900

    feat: 文字列操作関数の実装
    strcpy・strcmp関数の追加

--- common.c
+++ common.c
@@ -76,3 +76,23 @@ void *memset(void *buf, char c, size_t n) {
     }
     return buf;
 }
+
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src) {
+        *d++ = *src++;
+    }
+    *d = '\0';
+    return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
