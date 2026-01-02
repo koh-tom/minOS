cc0f12535e50290fbdb15213424a8b12b46c6a06
koh-tom <223271102+koh-tom@users.noreply.github.com>
2026-01-02 23:31:32 +0900

    feat: 🎸 ページングの実装

--- kernel.c
+++ kernel.c
@@ -4,6 +4,7 @@
 // 外部シンボル
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
+extern char __kernel_base[];
 
 // プロセス管理
 struct process procs[PROCS_MAX];
@@ -177,6 +178,27 @@ __attribute__((naked)) void switch_context(uint32_t *prev_sp,
                        "ret\n");
 }
 
+// ページテーブルの設定
+void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
+  if (!is_aligned(vaddr, PAGE_SIZE)) {
+    PANIC("unaligned vaddr %x", vaddr);
+  }
+
+  if (!is_aligned(paddr, PAGE_SIZE)) {
+    PANIC("unaligned paddr %x", paddr);
+  }
+
+  uint32_t vpn1 = (vaddr >> 22) & 0x3FF;
+  if ((table1[vpn1] & PAGE_V) == 0) {
+    uint32_t pt_paddr = alloc_pages(1);
+    table1[vpn1] = ((pt_paddr / PAGE_SIZE) << 10) | PAGE_V;
+  }
+
+  uint32_t vpn0 = (vaddr >> 12) & 0x3FF;
+  uint32_t *table0 = (uint32_t *)((table1[vpn1] >> 10) * PAGE_SIZE);
+  table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | PAGE_V | flags;
+}
+
 // プロセス生成
 struct process *create_process(uint32_t pc) {
   struct process *proc = NULL;
@@ -207,9 +229,17 @@ struct process *create_process(uint32_t pc) {
   *--sp = 0;            // s0
   *--sp = (uint32_t)pc; // ra
 
+  uint32_t *page_table = (uint32_t *)alloc_pages(1);
+
+  for (paddr_t paddr = (paddr_t)__kernel_base; paddr < (paddr_t)__free_ram_end;
+       paddr += PAGE_SIZE) {
+    map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
+  }
+
   proc->pid = i + 1;
   proc->state = PROCS_RUNNABLE;
   proc->sp = (uint32_t)sp;
+  proc->page_table = page_table;
   return proc;
 }
 
@@ -228,9 +258,13 @@ void yield(void) {
   }
 
   __asm__ __volatile__(
+      "sfence.vma\n"
+      "csrw satp, %[satp]\n"
+      "sfence.vma\n"
       "csrw sscratch, %[sscratch]\n"
       :
-      : [sscratch] "r"((uint32_t)&next->stack[sizeof(next->stack)]));
+      : [satp] "r"(SATP_SV32 | ((uint32_t)next->page_table / PAGE_SIZE)),
+        [sscratch] "r"((uint32_t)&next->stack[sizeof(next->stack)]));
 
   struct process *prev = current_proc;
   current_proc = next;
--- kernel.h
+++ kernel.h
@@ -5,6 +5,13 @@
 #define PROCS_UNUSED 0
 #define PROCS_RUNNABLE 1
 
+#define SATP_SV32 (1u << 31)
+#define PAGE_V (1 << 0) // 有効化ビット
+#define PAGE_R (1 << 1) // 読み取り可能
+#define PAGE_W (1 << 2) // 書き込み可能
+#define PAGE_X (1 << 3) // 実行可能
+#define PAGE_U (1 << 4) // ユーザーレベル
+
 #define PANIC(fmt, ...)                                                        \
   do {                                                                         \
     printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);      \
@@ -68,5 +75,6 @@ struct process {
   int pid;
   int state;
   vaddr_t sp;
+  uint32_t *page_table;
   uint8_t stack[8192];
 };
