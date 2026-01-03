959717016e1a33e492a3951d5dba205574337359
koh-tom <223271102+koh-tom@users.noreply.github.com>
2026-01-03 21:28:21 +0900

    feat: 🎸 ユーザランドの実装・シェルのビルド

--- run.sh
+++ run.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -xue
 
+# ツールチェーンのパス
 QEMU=qemu-system-riscv32
+CC=clang
+OBJCOPY=llvm-objcopy
 
-# clangのパス (Ubuntuの場合は CC=clang)
-clang=clang
+# コンパイルオプション
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf \
+    -fno-stack-protector -ffreestanding -nostdlib"
 
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+# シェルをビルド
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
 
 # カーネルをビルド
-$clang $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c shell.bin.o
 
 # QEMUを起動
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
